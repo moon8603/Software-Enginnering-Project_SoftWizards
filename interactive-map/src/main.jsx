623fe95d243ebaa8b9f364abdf825df4adcd3e57
moon8603 <buikimngan2003@gmail.com>
2024-11-28 00:48:45 +0900

    [IS-03] Add maxZoom and reformat map (#8)
    * [IS-03] Create map using Leaflet
    
    * [IS-03] Add maxZoom, reformat

--- interactive-map/src/main.jsx
+++ interactive-map/src/main.jsx
@@ -1,8 +1,9 @@
 import { MantineProvider } from '@mantine/core';
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import '@mantine/core/styles.css'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import '@mantine/core/styles.css';
+import 'leaflet/dist/leaflet.css';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <MantineProvider>
