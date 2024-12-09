ec1357f6971adfb75e28f9a2d3e25eeabdb5750b
moon8603 <buikimngan2003@gmail.com>
2024-12-09 22:39:54 +0900

    preparing for login feature implementation

--- interactive-map/src/Routes.jsx
+++ interactive-map/src/Routes.jsx
@@ -5,14 +5,15 @@ import LoginBtn from "./components/LoginBtn";
 import LoginPage from "./pages/LoginPage";
 import ForumBtn from "./components/ForumBtn";
 import ForumPage from "./pages/ForumPage";
-import PostDetail from "./components/PostDetail";
+// import PostDetail from "./components/PostDetail";
 
 function MyRoute() {
   return (
     <Routes>
       {/* Route to redirect from "/" to "/main" */}
       <Route path="/" element={<Navigate to="/main" />} />
-
+      
+      
       {/* Route to the main page with Map, Filter, etc. */}
       <Route
         path="/main"
@@ -31,13 +32,14 @@ function MyRoute() {
         }
       />
 
+      
       <Route path="/loginpage" element={<LoginPage />} />
       <Route path="/forumpage" element={<ForumPage />}>
         {/* Post Detail Page as a nested route */}
-      <Route path="/forumpage/:id" element={<PostDetail />} />
+        {/* <Route path=":postId" element={<PostDetail />} /> */}
       </Route>
     </Routes>
   );
 }
 
-export default MyRoute;
+export default MyRoute
