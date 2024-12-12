71146cae12496cf6e177178223860d4379e02ae5
look516 <happycatcindy@gmail.com>
2024-12-12 21:05:34 +0900

    ADJUST DATABASE

---
+++ backend/migrations/20241211015528-add-link-to-amenity.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.sequelize.query(
+      `ALTER TABLE amenities
+      ADD COLUMN link varchar(1000) AFTER type;`
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Amenities', 'link');
+  }
+};
