a2be2005b5b46d482e966fafcf05f2bdd3606f17
moon8603 <buikimngan2003@gmail.com>
2024-12-14 17:35:40 +0900

    Edit README, add proposal-sow.md, delete some unused file

--- interactive-map/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const useStore = create((set) => ({
-  currentUser: '',
-  setCurrentUser: (user) => set(() => ({currentUser: user})),
-}));
-
-export default useStore;
